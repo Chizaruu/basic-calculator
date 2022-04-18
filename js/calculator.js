e4b6826fbad426f64fa741c36a1f8a30ea8263fa
Chizaruu <AbdulCoskun@hotmail.com>
2022-04-18 12:44:06 +1000

    Calculator prototype

---
+++ js/calculator.js
@@ -0,0 +1,20 @@
+const addition = (a, b) => a + b;
+const subtraction = (a, b) => a - b;
+const multiplication = (a, b) => a * b;
+const division = (a, b) => a / b;
+
+
+const calculator = (a, b, operator) => {
+    switch (operator) {
+        case '+':
+            return addition(a, b);
+        case '-':
+            return subtraction(a, b);
+        case '*':
+            return multiplication(a, b);
+        case '/':
+            return division(a, b);
+        default:
+            return 'Invalid operator';
+    }
+}
