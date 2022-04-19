6b547569bde617fc15f866c00397a807bbf1e8e2
Chizaruu <AbdulCoskun@hotmail.com>
2022-04-19 19:30:57 +1000

    Refactored code to be cleaner

--- js/calculator.js
+++ js/calculator.js
@@ -34,7 +34,7 @@ const operation = (a, b, operator) => {
     }
 }
 
-const checkOperation = (operator) => {
+const checkOperation = () => {
     if (firstNumber !== 0 && secondNumber !== 0) {
         let result = operation(firstNumber, secondNumber, operator);
         display.value = result;
@@ -83,157 +83,36 @@ const addNumber = (number) => {
     Button click events
 */
 
-const addbtn = document.getElementById('add');
-
-addbtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    operator = '+';
-
-    checkOperation(operator);
-});
+const numbers = document.querySelectorAll('.number');
+numbers.forEach(number => number.addEventListener('click', () => addNumber(number.innerHTML)));
 
 
-const subbtn = document.getElementById('sub');
-
-subbtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    operator = '-';
-
-    checkOperation(operator);
-});
+const operators = document.querySelectorAll('.operator');
+operators.forEach(operator => operator.addEventListener('click', () => {
+    operator = operator.innerHTML;
+    checkOperation();
+}));
 
 
-const mulbtn = document.getElementById('mul');
-
-mulbtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    operator = '*';
-
-    checkOperation(operator);
-});
+const clearButton = document.getElementById('clear');
+clearButton.addEventListener('click', clear);
 
 
-const divbtn = document.getElementById('div');
-
-divbtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    operator = '/';
-
-    checkOperation(operator);
-});
-
-const changeSign = document.getElementById('change-sign');
-
-changeSign.addEventListener('click', (e) => {
-    e.preventDefault();
-    if (display.value !== '0') {
-        display.value = display.value * -1;
-    }
-});
+const equals = document.getElementById('equals');
+equals.addEventListener('click', calculate);
 
 
-const clearbtn = document.getElementById('clear');
-
-clearbtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    clear();
-});
-
-
-const equalsbtn = document.getElementById('equals');
-
-equalsbtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    calculate();
-});
-
-
-const dotbtn = document.getElementById('dot');
-
-dotbtn.addEventListener('click', (e) => {
-    e.preventDefault();
+const decimal = document.getElementById('decimal');
+decimal.addEventListener('click', () => {
     if (display.value.indexOf('.') === -1) {
         display.value += '.';
     }
 });
 
 
-const num0btn = document.getElementById('num0');
-
-num0btn.addEventListener('click', (e) => {
-    e.preventDefault();
-    addNumber('0');
-});
-
-
-const num1btn = document.getElementById('num1');
-
-num1btn.addEventListener('click', (e) => {
-    e.preventDefault();
-    addNumber('1');
-});
-
-
-const num2btn = document.getElementById('num2');
-
-num2btn.addEventListener('click', (e) => {
-    e.preventDefault();
-    addNumber('2');
-});
-
-
-const num3btn = document.getElementById('num3');
-
-num3btn.addEventListener('click', (e) => {
-    e.preventDefault();
-    addNumber('3');
-});
-
-
-const num4btn = document.getElementById('num4');
-
-num4btn.addEventListener('click', (e) => {
-    e.preventDefault();
-    addNumber('4');
-});
-
-
-const num5btn = document.getElementById('num5');
-
-num5btn.addEventListener('click', (e) => {
-    e.preventDefault();
-    addNumber('5');
-});
-
-
-const num6btn = document.getElementById('num6');
-
-num6btn.addEventListener('click', (e) => {
-
-    e.preventDefault();
-    addNumber('6');
-});
-
-
-const num7btn = document.getElementById('num7');
-
-num7btn.addEventListener('click', (e) => {
-    e.preventDefault();
-    addNumber('7');
-});
-
-
-const num8btn = document.getElementById('num8');
-
-num8btn.addEventListener('click', (e) => {
-    e.preventDefault();
-    addNumber('8');
-});
-
-
-const num9btn = document.getElementById('num9');
-
-num9btn.addEventListener('click', (e) => {
-    e.preventDefault();
-    addNumber('9');
+const changeSign = document.getElementById('change-sign');
+changeSign.addEventListener('click', () => {
+    if (display.value !== '0') {
+        display.value = display.value * -1;
+    }
 });
